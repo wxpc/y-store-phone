1b01b03939eb7a091d2e05724aa3b5d9dea6b805
jacoblin214 <993043389@qq.com>
2017-12-29 18:56:49 +0800

    修复图片间距、及dpr3屏幕下边框不显示问题

--- js/navigation.js
+++ js/navigation.js
@@ -1,42 +1,42 @@
 /**
  * @Author: jecyu
- * @Date: 2017-12-19 1:53:37 pm 
- * @Modified By: jeCyu 
- * @Last Modified time: 2017-12-19 2:38:44 pm 
+ * @Date: 2017-12-19 1:53:37 pm
+ * @Modified By: jeCyu
+ * @Last Modified time: 2017-12-27 6:28:28 pm
  */
 
 var page = {
-    onload: function () {
+    onload: function() {
         this.init();
     },
-    init: function () {
-        this.bindEvent()
+    init: function() {
+        this.bindEvent();
     },
-    bindEvent: function () {
-
+    bindEvent: function() {
+        // 用于判断是否折叠
+        var isFolded = false;
         // expand or collaspe
-        $('.fold-header').click(function () {
+        $(".fold-header").click(function() {
+            // 取反
+            isFolded = !isFolded;
             $header = $(this);
             $content = $header.next();
-            $icon = $header.find('.iconfont');
+            $icon = $header.find(".iconfont");
 
-            $content.slideToggle(500, function () {
-
-                if ($icon.hasClass('icon-row-down-copy')) {
-                    $icon.removeClass('icon-row-down-copy');
-                    $icon.addClass('icon-row-up');
-                } 
-                else {
-                    $icon.removeClass('icon-row-up');
-                    $icon.addClass('icon-row-down-copy');
+            $content.slideToggle(500, function() {
+                // 展开
+                if (isFolded) {
+                    $icon.removeClass("icon-row-down-copy");
+                    $icon.addClass("icon-row-up");
+                } else {
+                    $icon.removeClass("icon-row-up");
+                    $icon.addClass("icon-row-down-copy");
                 }
             });
-
         });
-    },
-
+    }
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
     page.onload();
 });
