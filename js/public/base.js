93b1f38c7ae794bc5e623d66617fc81765d0ba9e
jacoblin214 <993043389@qq.com>
2018-01-06 09:36:55 +0800

    增加产地滑动、搜索下拉框

--- js/public/base.js
+++ js/public/base.js
@@ -1,11 +1,13 @@
 /**
  * @Author: jecyu
- * @Date: 2017-12-19 4:36:23 pm 
- * @Modified By: jeCyu 
- * @Last Modified time: 2017-12-19 4:38:44 pm 
+ * @Date: 2017-12-19 4:36:23 pm
+ * @Modified By: jeCyu
+ * @Last Modified time: 2018-01-05 5:25:54 pm
  */
 
-// back to top 
-$('#toTop').on('click', function() {
-    $('body, html').animate({scrollTop: 0}, 1000);
+// back to top
+$("#toTop").on("click", function() {
+    $("body, html").animate({ scrollTop: 0 }, 1000);
 });
+
+// popup search bar
